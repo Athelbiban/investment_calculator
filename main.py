a6d5531bce83f214266ff4f922eeac422817fbcf
athelbiban <vostrovso@gmail.com>
2025-05-08 13:09:57 +0300

    mod: main.py
        Добавлена проверка обновления отчетов брокера по выбору пользователя

--- main.py
+++ main.py
@@ -174,7 +174,12 @@ def get_coupon_data_dict(tickers: list):
 
 def main():
 
-    # mail_main()
+    resp = input('Обновить отчёты брокера?(y/n): ').strip()
+    if resp == ('y' or 'Y'):
+        mail_main()
+    elif resp != ('n' or 'N'):
+        raise Exception('Неверный ответ. Повторите попытку.')
+
     parser_main()
 
     portfolio = pd.read_csv('files/portfolio.csv')
