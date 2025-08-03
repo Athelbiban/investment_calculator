d2d58a8f0fa622376b5d570b5361806dddaeeb37
athelbiban <vostrovso@gmail.com>
2025-08-03 19:42:12 +0300

    mod: parser.py см.описание
        1. создает новую таблицу securities_move.csv
        2. исправлена ошибка при которой в файле csv не всегда создавался
           заголовок
        3. удалил метод portfolio() за ненадобностью файла portfolio.csv
        4. общий рефакторинг, например добавлен новый метод-прослойка
           my_writer()

--- parser.py
+++ parser.py
@@ -2,52 +2,30 @@ import csv
 import os
 import re
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 from pathlib import Path
 from directing import get_directory
 
 
-def get_portfolio(input_files, output_file, header_flag=True, date=None):
-
-    header = [
-        'Наименование', 'ISIN', 'Валюта', 'Количество_нп', 'Номинал_нп',
-        'Цена_нп', 'Стоимость_нп', 'НКД_нп', 'Количество_кп',
-        'Номинал_кп', 'Цена_кп', 'Стоимость_кп', 'НКД_кп', 'Количество_изп',
-        'Стоимость_изп', 'Зачисления', 'Списания', 'Остаток', 'Дата'
-    ]
-
-    reg_table_start = re.compile(r'Портфель Ценных Бумаг')
-    reg_table_finish = re.compile(r'Итого по Основному рынку, RUB')
-    keywords_list = [
-        '', 'Основной рынок', 'Наименование',
-        'Площадка: Фондовый рынок', '1', 'Блокировано'
-    ]
-
-    for file, date in zip(input_files, date):
-        write_file(file, output_file, header, reg_table_start,
-                   reg_table_finish, keywords_list, header_flag, date)
-        header_flag = False
-
-
-def get_transactions(input_files, output_file, header_flag=True):
+def get_transactions(input_files: list[str], output_file: str,
+                     header_flag=True):
 
     header = [
         'Дата заключения', 'Дата расчетов', 'Время заключения', 'Наименование',
         'Код', 'Валюта', 'Вид', 'Количество', 'Цена', 'Сумма', 'НКД',
-        'Комиссия Брокера', 'Комиссия Биржи', 'Номер сделки', 'Комментарий', 'Статус'
+        'Комиссия Брокера', 'Комиссия Биржи', 'Номер сделки',
+        'Комментарий', 'Статус'
     ]
 
     reg_table_start = re.compile(r'Сделки купли/продажи')
     reg_table_finish = re.compile(r'Итого, RUB')
-    keywords_list = ['Дата заключения', 'Площадка: Фондовый рынок', '1']
+    stopwords_list = ['Дата заключения', 'Площадка: Фондовый рынок', '1']
 
-    for file in input_files:
-        write_file(file, output_file, header, reg_table_start,
-                   reg_table_finish, keywords_list, header_flag)
-        header_flag = False
+    my_writer(input_files, output_file, header, reg_table_start,
+              reg_table_finish, stopwords_list, header_flag)
 
 
-def get_cashflow(input_files, output_file, header_flag=True):
+def get_cashflow(input_files: list[str], output_file: str, header_flag=True):
 
     header = [
         'Дата', 'Торговая площадка', 'Описание операции',
@@ -56,77 +34,99 @@ def get_cashflow(input_files, output_file, header_flag=True):
 
     reg_table_start = re.compile(r'Движение денежных средств за период')
     reg_table_finish = re.compile(r'Итого, RUB')
-    keywords_list = ['Дата', '1']
+    stopwords_list = ['Дата', '1']
 
-    for file in input_files:
-        write_file(file, output_file, header, reg_table_start,
-                   reg_table_finish, keywords_list, header_flag)
-        header_flag = False
+    my_writer(input_files, output_file, header, reg_table_start,
+              reg_table_finish, stopwords_list, header_flag)
 
 
-def write_file(file, output_file, header, reg_table_start, reg_table_finish,
-               keywords_list, header_flag, date=None):
+def get_securities_movement(input_files: list[str], output_file: str,
+                            header_flag=True):
 
-    # Поиск пробелов в числе, например '1 504.24'
-    reg = re.compile(r'^\d+\s+\d+\s*\d*\.?\d*')
+    header = [
+        'Дата операции', 'Наименование ЦБ', 'Код ЦБ', 'Вид',
+        'Основание операции', 'Количество, шт', 'Дата приобретения', 'Цена',
+        'Комиссия Брокера, руб', 'Комиссия Биржи, руб', 'Другие затраты'
+    ]
 
-    with (open(file, encoding='utf-8') as inf, \
-            open(output_file, 'a', encoding='utf-8', newline='') as ouf):
+    reg_table_start = re.compile(
+        r'Движение ЦБ, не связанное с исполнением сделок'
+    )
+    reg_table_finish = re.compile(r'Итого по площадке Фондовый рынок')
+    stopwords_list = ['', 'Дата операции', '1', 'Площадка: Фондовый рынок']
+
+    my_writer(input_files, output_file, header, reg_table_start,
+              reg_table_finish, stopwords_list, header_flag)
+
+
+def write_file(file: str, output_file: str, header: list[str],
+               reg_table_start: re.Pattern, reg_table_finish: re.Pattern,
+               reg_numbers: re.Pattern, stopwords_list: list[str],
+               header_flag: bool):
+
+    with (open(file, encoding='utf-8') as inf,
+          open(output_file, 'a', encoding='utf-8', newline='') as ouf):
 
         writer = csv.writer(ouf)
-        soup = BeautifulSoup(inf.read(), 'lxml').select('tr, p')
+        soup: element.ResultSet = BeautifulSoup(
+            inf.read(), 'lxml').select('tr, p')
         table_flag = False
-        for string in soup:
 
-            if re.search(reg_table_start, string.text):
+        for element_Tag in soup:
+
+            if header_flag:
+                writer.writerow(header)
+                header_flag = False
+
+            if re.search(reg_table_start, element_Tag.text):
                 table_flag = True
-            elif re.search(reg_table_finish, string.text):
+            elif re.search(reg_table_finish, element_Tag.text):
                 table_flag = False
 
             if table_flag:
-                string = string.find_all('td')
+                _resultSet: element.ResultSet = element_Tag.find_all('td')
 
-                if header_flag:
-                    writer.writerow(header)
-                    header_flag = False
-
-                if string and string[0].text not in keywords_list:
-                    if date:
-                        writer.writerow([re.sub(reg, elem.text.replace(' ', ''), elem.text)
-                                         for elem in string] + [date])
-                    else:
-                        writer.writerow(re.sub(reg, elem.text.replace(' ', ''), elem.text)
-                                        for elem in string)
+                if _resultSet and _resultSet[0].text not in stopwords_list:
+                    writer.writerow(re.sub(
+                        reg_numbers,
+                        elem.text.replace(' ', ''),
+                        elem.text
+                    ) for elem in _resultSet)
 
 
-def parse_directory(directory):
+def my_writer(input_files, output_file, header, reg_table_start,
+              reg_table_finish, stopwords_list, header_flag):
+
+    # Поиск пробелов в числе, например '1 504.24'
+    reg_numbers = re.compile(r'^\d+\s+\d+\s*\d*\.?\d*')
+
+    for file in input_files:
+        write_file(file, output_file, header, reg_table_start,
+                   reg_table_finish, reg_numbers, stopwords_list, header_flag)
+        if header_flag:
+            header_flag = False
+
+
+def parse_directory(directory: str):
     return [f'{directory}{node}' for node in sorted(os.listdir(directory))]
 
 
 def main():
 
-    directory = get_directory()
-    paths = parse_directory(directory)
-    out_file_1, out_file_2 = ['files/cashflow.csv', 'files/transactions.csv']
+    directory: str = get_directory()
+    paths: list[str] = parse_directory(directory)
+    out_file_1 = 'files/transactions.csv'
+    out_file_2 = 'files/cashflow.csv'
+    out_file_3 = 'files/securities_move.csv'
 
-    for f in out_file_1, out_file_2:
+    for f in out_file_1, out_file_2, out_file_3:
         Path(f).unlink(missing_ok=True)
 
     os.makedirs('files', exist_ok=True)
 
-    date: list[str] = ['']  # format date '2022-01-31'
-    for path in paths:
-
-        path_split = path.split('.')[0].split('_')
-        if path_split[-1].isdigit():
-            date = [f'20{i[-2:]}-{i[2:4]}-{i[:2]}' for i in [path_split[-1]]]
-        elif path_split[-2].isdigit():
-            date = [f'20{i[-2:]}-{i[2:4]}-{i[:2]}' for i in [path_split[-2]]]
-        else:
-            raise Exception('Неверное имя файла: ' + path)
-
-    get_cashflow(paths, out_file_1)
-    get_transactions(paths, out_file_2)
+    get_transactions(paths, out_file_1)
+    get_cashflow(paths, out_file_2)
+    get_securities_movement(paths, out_file_3)
 
 
 if __name__ == '__main__':
