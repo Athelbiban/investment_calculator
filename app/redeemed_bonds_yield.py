cc4614b18860fc31d7a2517ad7cd115c234c1609
athelbiban <vostrovso@gmail.com>
2025-08-25 20:50:28 +0300

    fix: app/redeemed_bonds_yield.py

--- app/redeemed_bonds_yield.py
+++ app/redeemed_bonds_yield.py
@@ -2,113 +2,107 @@ import psycopg2
 import re
 
 from decimal import Decimal
-from passwd.config_DB import dbname, user, password
+from passwd.config_DB import connect_attr
 
 
-def main():
+def get_redeemed_bonds():
 
-    conn = psycopg2.connect(
-        database=dbname,
-        user=user,
-        password=password
-    )
+    with psycopg2.connect(connect_attr()) as conn:
+        with conn.cursor() as cursor:
 
-    with conn.cursor() as cursor:
-
-        income_list =[]
-
-        cursor.execute(
-            '''
-            select date, description_operation, transfer_amount
-            from cashflow
-            where description_operation ~* '^зачисление д/с'
-                and description_operation ~* 'погашение'
-            '''
-        )
-
-        redeemed_bonds_tuples_list = cursor.fetchall()
-        rb_list = []
-        reg = re.compile(r"Зачисление д/с \(погашение ")
-
-        for i in redeemed_bonds_tuples_list:
-            bond = re.sub(reg, '', i[1][:-1])
-            rb_list.append([bond, i[0], i[2]])
-
-        for i, bond_list in enumerate(rb_list):
-            bond_name = bond_list[0]
+            income_list =[]
 
             cursor.execute(
-                f'''
-                select sum(summ)
-                from transactions
-                where product_title = '{bond_name}'
-                    and status = 'И'
-                    and transaction_type ~* 'покупка'
+                '''
+                select trading_date, description_operation, transfer_amount
+                from cashflows
+                where description_operation ~* '^зачисление д/с'
+                    and description_operation ~* 'погашение'
                 '''
             )
 
-            purchase_price = cursor.fetchone()[0]
-            rb_list[i].append(purchase_price)
+            redeemed_bonds_tuples_list = cursor.fetchall()
+            rb_list = []
+            reg = re.compile(r"Зачисление д/с \(погашение ")
 
-            cursor.execute(
-                f'''
-                select sum(transfer_amount)
-                from cashflow
-                where description_operation ~* '^зачисление д/с' 
-                    and description_operation ~* 'купон'
-                    and description_operation ~* '{bond_name}';
-                '''
-            )
+            for i in redeemed_bonds_tuples_list:
+                bond = re.sub(reg, '', i[1][:-1])
+                rb_list.append([bond, i[0], i[2]])
 
-            coupons = cursor.fetchone()[0]
-            # print(coupons)
-            rb_list[i].append(coupons)
+            for i, bond_list in enumerate(rb_list):
+                bond_name = bond_list[0]
 
-            cursor.execute(
-                f'''
-                select conclusion_date, 
-                    sum(broker_commission),
-                    sum(exchange_commission)
-                from transactions t 
-                where transaction_type ~* 'покупка' 
-                    and product_code ~* '^ru' 
-                    and status = 'И'
-                    and t.product_title = '{bond_name}'
-                group by conclusion_date; 
-                '''
-            )
+                cursor.execute(
+                    f'''
+                    select sum(summ)
+                    from transactions
+                    where product_title = '{bond_name}'
+                        and status = 'И'
+                        and transaction_type ~* 'покупка'
+                    '''
+                )
 
-            conclusion_date, broker_comm, exchange_comm = cursor.fetchone()
-            # !здесь берем только первую дату, если были докупки, то надо по-другому
-            rb_list[i].append(conclusion_date)
-            rb_list[i].append(broker_comm)
-            rb_list[i].append(exchange_comm)
-            rb_list[i].append(
-                (rb_list[i][2] - rb_list[i][3]) *
-                Decimal('0.13')
-            )
+                purchase_price = cursor.fetchone()[0]
+                rb_list[i].append(purchase_price)
 
-            if rb_list[i][4]:
-                net_profit_rub = rb_list[i][2] + rb_list[i][4] - rb_list[i][3] \
-                                 - rb_list[i][6] - rb_list[i][7] - rb_list[i][8]
-            else:
-                net_profit_rub = rb_list[i][2] - rb_list[i][3] \
-                                 - rb_list[i][6] - rb_list[i][7] - rb_list[i][8]
+                cursor.execute(
+                    f'''
+                    select sum(transfer_amount)
+                    from cashflows
+                    where description_operation ~* '^зачисление д/с' 
+                        and description_operation ~* 'купон'
+                        and description_operation ~* '{bond_name}';
+                    '''
+                )
 
-            days_delta = (rb_list[i][1] - rb_list[i][5]).days
-            net_profit_percent = net_profit_rub * 100 / \
-                                 rb_list[i][3] / days_delta * 365
-            income_list.append([rb_list[i][0], round(net_profit_percent, 2),
-                                rb_list[i][1], days_delta, rb_list[i][3],
-                                round(net_profit_rub, 2)])
+                coupons = cursor.fetchone()[0]
+                # print(coupons)
+                rb_list[i].append(coupons)
 
-    conn.close()
+                cursor.execute(
+                    f'''
+                    select conclusion_date, 
+                        sum(broker_commission),
+                        sum(exchange_commission)
+                    from transactions t 
+                    where transaction_type ~* 'покупка' 
+                        and product_code ~* '^ru' 
+                        and status = 'И'
+                        and t.product_title = '{bond_name}'
+                    group by conclusion_date; 
+                    '''
+                )
 
-    for i in income_list:
-        print('Название: ' + i[0] + ';' + ' % годовых: ' + str(i[1]) + ';' +
-              ' Дата погашения: ' + str(i[2]) + ';' + ' Дни: ' + str(i[3]) +
-              ';' + ' Вложено: ' + str(i[4]) + ' Получено: ' + str(i[5])
-              )
+                conclusion_date, broker_comm, exchange_comm = cursor.fetchone()
+                # !здесь берем только первую дату, если были докупки, то надо по-другому
+                rb_list[i].append(conclusion_date)
+                rb_list[i].append(broker_comm)
+                rb_list[i].append(exchange_comm)
+                rb_list[i].append(
+                    (rb_list[i][2] - rb_list[i][3]) *
+                    Decimal('0.13')
+                )
+
+                if rb_list[i][4]:
+                    net_profit_rub = rb_list[i][2] + rb_list[i][4] - rb_list[i][3] \
+                                     - rb_list[i][6] - rb_list[i][7] - rb_list[i][8]
+                else:
+                    net_profit_rub = rb_list[i][2] - rb_list[i][3] \
+                                     - rb_list[i][6] - rb_list[i][7] - rb_list[i][8]
+
+                days_delta = (rb_list[i][1] - rb_list[i][5]).days
+                net_profit_percent = net_profit_rub * 100 / \
+                                     rb_list[i][3] / days_delta * 365
+                income_list.append([rb_list[i][0], round(net_profit_percent, 2),
+                                    rb_list[i][1], days_delta, rb_list[i][3],
+                                    round(net_profit_rub, 2)])
+
+        for i in income_list:
+            print('Название: ' + i[0] + ';' + ' % годовых: ' + str(i[1]) + ';' +
+                  ' Дата погашения: ' + str(i[2]) + ';' + ' Дни: ' + str(i[3]) +
+                  ';' + ' Вложено: ' + str(i[4]) + ' Получено: ' + str(i[5])
+                  )
+
 
 if __name__ == '__main__':
-    main()
+    get_redeemed_bonds()
