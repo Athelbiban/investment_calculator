8a51f9de137df7c7fd137b8a486d64d619426a7c
athelbiban <vostrovso@gmail.com>
2026-05-12 19:49:01 +0300

    mod: main.py
        добавлена возможность выбрать полное автоматическое обновление или
        обновиться пошагово в ручном режиме

--- main.py
+++ main.py
@@ -8,62 +8,85 @@ from app.writer_gsheets import main as w_gsheets
 
 def main():
 
-    print('Investment Calculator v0.3.2 by Stas Vostrov\n')
-    resp = input('Обновить отчёты брокера?(y/n): ').strip().lower()
-    if resp == 'y' or 'н':
+    print('Investment Calculator v0.4.0 by Stas Vostrov\n')
+
+    response = input('Выполнить полное обновление автоматически(y)\n'
+                     'Выполнить действия по шагам самостоятельно(n): ').strip().lower()
+    if response == 'y' or 'н':
         start_animation_func()
         try:
             get_reports()
-        finally:
-            stop_animation_func()
-        print('Обновление отчетов завершено')
-    elif resp == 'n' or 'т':
-        print('Отчёты не обновлены')
-    elif resp != 'n' or 'т':
-        print('Неверный ответ. Отчёты не обновлены')
-
-    resp1 = input('Обновить csv-файлы?(y/n): ').strip().lower()
-    if resp == 'y' or 'н':
-        start_animation_func()
-        try:
             launch_parser()
             build_general_portfolio()
-        finally:
-            stop_animation_func()
-        print('Обновление файлов-csv завершено')
-    elif resp1 == 'n' or 'т':
-        print('Файлы-csv не обновлены: отменено пользователем')
-    elif resp1 != 'n' or 'т':
-        print('Неверный ответ. файлы-csv не обновлены')
-
-    resp2 = (input('Обновить базу данных?(y/n): ')
-             .strip().lower())
-    if resp == 'y' or 'н':
-        start_animation_func()
-        try:
             recreate_database()
-        finally:
-            stop_animation_func()
-        print('Обновление базы завершено')
-    elif resp2 == 'n' or 'т':
-        print('База не обновлена: отменено пользователем')
-    else:
-        print('Неверный ответ. База не обновлена')
-
-    resp3 = input('Обновить Google Таблицу в облаке?(y/n): ').strip().lower()
-    if resp3 == 'y' or 'н':
-        start_animation_func()
-        try:
             w_gsheets()
+        except Exception as e:
+            print(f"Непредвиденная ошибка: {e}")
+            input('\nНажмите Enter для выхода...')
         finally:
             stop_animation_func()
-        print('Обновление Google Таблицы завершено')
-    elif resp3 == 'n' or 'т':
-        print('Google Таблица не обновлена: отменено пользователем')
-    else:
-        print('Неверный ответ. Google Таблица не обновлена')
+        print('Полное обновление выполнено')
+        input('\nУспешное выполнение. Нажмите Enter для выхода...')
+    elif response == 'n' or 'т':
 
-    input('\nУспешное выполнение. Нажмите Enter для выхода...')
+        resp = input('Обновить отчёты брокера?(y/n): ').strip().lower()
+        if resp == 'y' or 'н':
+            start_animation_func()
+            try:
+                get_reports()
+            finally:
+                stop_animation_func()
+            print('Обновление отчетов завершено')
+        elif resp == 'n' or 'т':
+            print('Отчёты не обновлены')
+        elif resp != 'n' or 'т':
+            print('Неверный ответ. Отчёты не обновлены')
+
+        resp1 = input('Обновить csv-файлы?(y/n): ').strip().lower()
+        if resp == 'y' or 'н':
+            start_animation_func()
+            try:
+                launch_parser()
+                build_general_portfolio()
+            finally:
+                stop_animation_func()
+            print('Обновление файлов-csv завершено')
+        elif resp1 == 'n' or 'т':
+            print('Файлы-csv не обновлены: отменено пользователем')
+        elif resp1 != 'n' or 'т':
+            print('Неверный ответ. файлы-csv не обновлены')
+
+        resp2 = (input('Обновить базу данных?(y/n): ')
+                 .strip().lower())
+        if resp == 'y' or 'н':
+            start_animation_func()
+            try:
+                recreate_database()
+            finally:
+                stop_animation_func()
+            print('Обновление базы завершено')
+        elif resp2 == 'n' or 'т':
+            print('База не обновлена: отменено пользователем')
+        else:
+            print('Неверный ответ. База не обновлена')
+
+        resp3 = input('Обновить Google Таблицу в облаке?(y/n): ').strip().lower()
+        if resp3 == 'y' or 'н':
+            start_animation_func()
+            try:
+                w_gsheets()
+            finally:
+                stop_animation_func()
+            print('Обновление Google Таблицы завершено')
+        elif resp3 == 'n' or 'т':
+            print('Google Таблица не обновлена: отменено пользователем')
+        else:
+            print('Неверный ответ. Google Таблица не обновлена')
+
+        input('\nУспешное выполнение. Нажмите Enter для выхода...')
+    elif response != 'n' or 'т':
+        print('Неверный ответ. Обновление не выполнено')
+
 
 
 if __name__ == '__main__':
