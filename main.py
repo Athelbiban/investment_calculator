1f02a63a12267b557ea9cfa1dfe378e229da2641
athelbiban <vostrovso@gmail.com>
2026-05-12 21:49:26 +0300

    mod: v0.5.0
        введено управление консольными командами
    
    	modified:   main.py

--- main.py
+++ main.py
@@ -6,87 +6,90 @@ from app.portfolio_accountant import build_general_portfolio
 from app.writer_gsheets import main as w_gsheets
 
 
+
+def main_response():
+    return input('\nВведите команду (список команд help): ').strip().lower()
+
+
+def main_help():
+    print('\nhelp - список команд'
+          '\nall - выполнить все действия автоматически'
+          '\nupdreports - обновить отчеты брокера'
+          '\nupdcsv - обновить csv-файлы'
+          '\nupddb - обновить базу данных'
+          '\nupdgsheets - обновить Google-таблицу в облаке')
+
+
 def main():
 
-    print('Investment Calculator v0.4.0 by Stas Vostrov\n')
+    print('Investment Calculator v0.5.0 by Stas Vostrov\n')
 
-    response = input('Выполнить полное обновление автоматически(y)\n'
-                     'Выполнить действия по шагам самостоятельно(n): ').strip().lower()
-    if response == 'y' or 'н':
-        start_animation_func()
-        try:
-            get_reports()
-            launch_parser()
-            build_general_portfolio()
-            recreate_database()
-            w_gsheets()
-        except Exception as e:
-            print(f"Непредвиденная ошибка: {e}")
-            input('\nНажмите Enter для выхода...')
-        finally:
-            stop_animation_func()
-        print('Полное обновление выполнено')
-        input('\nУспешное выполнение. Нажмите Enter для выхода...')
-    elif response == 'n' or 'т':
+    command_list = ['help', 'all', 'updreports', 'updcsv', 'upddb', 'updgsheets', 'exit',
+                    'рудз', 'фдд', 'гзвкузщкеы', 'гзвсым', 'гзвви', 'гзвпырууеы', 'учше'
+    ]
 
-        resp = input('Обновить отчёты брокера?(y/n): ').strip().lower()
-        if resp == 'y' or 'н':
+    while (response := main_response()) != 'exit':
+
+        if response == 'help' or response == 'рудз':
+            main_help()
+            continue
+
+        elif response == 'all' or response == 'фдд':
+            start_animation_func()
+            try:
+                get_reports()
+                launch_parser()
+                build_general_portfolio()
+                recreate_database()
+                w_gsheets()
+            except Exception as e:
+                print(f"\nНепредвиденная ошибка: {e}")
+                input('\nНажмите Enter для выхода...')
+            finally:
+                stop_animation_func()
+            print('Полное обновление выполнено\n')
+            continue
+
+        elif response == 'updreports' or response == 'гзвкузщкеы':
             start_animation_func()
             try:
                 get_reports()
             finally:
                 stop_animation_func()
-            print('Обновление отчетов завершено')
-        elif resp == 'n' or 'т':
-            print('Отчёты не обновлены')
-        elif resp != 'n' or 'т':
-            print('Неверный ответ. Отчёты не обновлены')
+            print('Обновление отчетов завершено\n')
+            continue
 
-        resp1 = input('Обновить csv-файлы?(y/n): ').strip().lower()
-        if resp == 'y' or 'н':
+        elif response == 'updcsv' or response == 'гзвсым':
             start_animation_func()
             try:
                 launch_parser()
                 build_general_portfolio()
             finally:
                 stop_animation_func()
-            print('Обновление файлов-csv завершено')
-        elif resp1 == 'n' or 'т':
-            print('Файлы-csv не обновлены: отменено пользователем')
-        elif resp1 != 'n' or 'т':
-            print('Неверный ответ. файлы-csv не обновлены')
+            print('Обновление файлов-csv завершено\n')
+            continue
 
-        resp2 = (input('Обновить базу данных?(y/n): ')
-                 .strip().lower())
-        if resp == 'y' or 'н':
+        elif response == 'upddb' or response == 'гзвви':
             start_animation_func()
             try:
                 recreate_database()
             finally:
                 stop_animation_func()
-            print('Обновление базы завершено')
-        elif resp2 == 'n' or 'т':
-            print('База не обновлена: отменено пользователем')
-        else:
-            print('Неверный ответ. База не обновлена')
+            print('Обновление базы завершено\n')
+            continue
 
-        resp3 = input('Обновить Google Таблицу в облаке?(y/n): ').strip().lower()
-        if resp3 == 'y' or 'н':
+        elif response == 'updgsheets' or response == 'гзвпырууеы':
             start_animation_func()
             try:
                 w_gsheets()
             finally:
                 stop_animation_func()
-            print('Обновление Google Таблицы завершено')
-        elif resp3 == 'n' or 'т':
-            print('Google Таблица не обновлена: отменено пользователем')
+            print('Обновление Google Таблицы завершено\n')
+            continue
+
         else:
-            print('Неверный ответ. Google Таблица не обновлена')
-
-        input('\nУспешное выполнение. Нажмите Enter для выхода...')
-    elif response != 'n' or 'т':
-        print('Неверный ответ. Обновление не выполнено')
-
+            print('Неверный ответ\n')
+            continue
 
 
 if __name__ == '__main__':
